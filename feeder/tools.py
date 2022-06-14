8112d4780a8f3dc6b6405c9691b4b60df34c9c3b
ruczhouyujie <47097735+ruczhouyujie@users.noreply.github.com>
2022-06-14 19:56:57 +0800

    zyj

--- feeder/tools.py
+++ feeder/tools.py
@@ -37,3 +37,23 @@ def reverse(data_numpy, p=0.5):
         return data_numpy[:, time_range_reverse, :, :]
     else:
         return data_numpy
+
+def motion_resample(data_numpy, max_frame=50, resample_frame=30):
+
+    temp = data_numpy
+    c, t, v, m = temp.shape
+    motion = torch.zeros_like(temp)
+    motion[:, :-1, :, :] = temp[:, 1:, :, :] - temp[:, :-1, :, :]
+    motion = motion**2
+    temporal_att = motion.mean((0,2,3))
+    _,temp_list = torch.topk(temporal_att, resample_frame)
+    temp_list,_ = torch.sort(temp_list.squeeze())
+    temp_list = repeat(temp_list,'t -> c t v m',c=c,v=v,m=m)
+    output = temp.gather(1,temp_list)#[n c 30 v m]
+    output = rearrange(output,'c t v m -> c (v m) t')
+    output = output[:, :, None]
+    output = F.interpolate(output, size=(max_frame, 1), mode='bilinear',align_corners=False)
+    output = output.squeeze(dim=-1)
+    output = rearrange(output,'c (v m) t -> c t v m',c=c,v=v,m=m)
+
+    return output
