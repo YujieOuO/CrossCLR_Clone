3eb6eca1c34342a432a50f770792b936ea234711
ruczhouyujie <47097735+ruczhouyujie@users.noreply.github.com>
2022-08-04 12:09:37 +0800

    zyj

--- processor/pretrain_skeletonclr.py
+++ processor/pretrain_skeletonclr.py
@@ -69,7 +69,12 @@ class SkeletonCLR_Processor(PT_Processor):
                 raise ValueError
 
             # forward
-            loss = self.model(data1, data2)
+            output, target = self.model(data1, data2)
+            if hasattr(self.model, 'module'):
+                self.model.module.update_ptr(output.size(0))
+            else:
+                self.model.update_ptr(output.size(0))
+            loss = self.loss(output, target)
 
             # backward
             self.optimizer.zero_grad()
