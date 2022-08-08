1062eddf735b8e393930c0c43edf335e3a4e716b
ruczhouyujie <47097735+ruczhouyujie@users.noreply.github.com>
2022-08-08 20:58:29 +0800

    zyj

--- net/skeletonclr.py
+++ net/skeletonclr.py
@@ -129,7 +129,7 @@ class SkeletonCLR(nn.Module):
         """
 
         # ignore_joint = random.sample(range(25), 10)
-        ignore_joint = self.get_ignore_joint2(4)
+        ignore_joint = self.get_ignore_joint3()
 
         if not self.pretrain:
             return self.encoder_q(im_q, ignore_joint)
