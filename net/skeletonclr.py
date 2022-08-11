a9e43fdf7213bf444c9e66a56f85f540d39bcb94
ruczhouyujie <47097735+ruczhouyujie@users.noreply.github.com>
2022-08-11 12:15:48 +0800

    zyj

--- net/skeletonclr.py
+++ net/skeletonclr.py
@@ -132,6 +132,7 @@ class SkeletonCLR(nn.Module):
         # ignore_joint = self.get_ignore_joint3()
 
         if not self.pretrain:
+            im_q = im_q[:, :, :25, :, :]
             return self.encoder_q(im_q)
 
         # compute query features
