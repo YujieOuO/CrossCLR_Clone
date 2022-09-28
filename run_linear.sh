9554c868bbf221aa876a64f06203ca9c71ebe158
ruczhouyujie <47097735+ruczhouyujie@users.noreply.github.com>
2022-09-28 19:11:42 +0800

    zyj

--- run_linear.sh
+++ run_linear.sh
@@ -18,6 +18,5 @@ srun -p ${PARTITION} \
     --cpus-per-task=${CPUS_PER_TASK} \
     --kill-on-bad-exit=1 \
     --quotatype=auto \
-    -x SH-IDC1-10-140-0-229\
     ${SRUN_ARGS} \
     python -u main.py linear_evaluation --config config/linear_eval/linear_skeletonclr.yaml
